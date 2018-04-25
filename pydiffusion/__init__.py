f027f3f09f6a9e233edf4a4e1921f335c6d6f636
Zhangqi Chen <wshchzhq@gmail.com>
2018-04-24 22:25:39 -0400

    version=0.1.2

--- pydiffusion/__init__.py
+++ pydiffusion/__init__.py
@@ -3,7 +3,7 @@ pyDiffusion combines tools like diffusion simulation, diffusion data smooth,
 forward simulation analysis (FSA), etc. to help people analyze diffusion data
 efficiently.
 """
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from pydiffusion.core import DiffProfile, DiffSystem
 from pydiffusion.simulation import mphSim
