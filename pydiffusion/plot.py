0ed22a0b8adfce40a128deb26003164f698d269d
Zhangqi Chen <wshchzhq@gmail.com>
2018-06-11 16:47:15 -0400

    Delete default color of plotting

--- pydiffusion/plot.py
+++ pydiffusion/plot.py
@@ -8,6 +8,36 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import splev
 from pydiffusion.Dtools import SF
 
+# Default label font size
+label_fontsize = 13
+# Default tick font size
+tick_fontsize = 11
+
+
+def plot_lim(x1, x2, log=False):
+    """
+    Automatically generate plot range for diffusion plots
+
+    Parameters
+    ----------
+    x1, x2 : float
+        Plot range, can be concentration or diffusivity values.
+    log : bool, optional
+        Range in log scale or not.
+
+    Returns
+    -------
+    x1_lim, x2_lim : float
+        Plot range output, used for plt.xlim or plt.set_ylim.
+    """
+    if not log:
+        x1_lim = (x1*100//1)/100
+        x2_lim = (x2*100//1+1)/100
+    else:
+        x1_lim = 10**np.floor(np.log10(x1))
+        x2_lim = 10**(np.floor(np.log10(x2))+1)
+    return x1_lim, x2_lim
+
 
 def profileplot(profile, ax=None, err=None, **kwargs):
     """
@@ -23,7 +53,7 @@ def profileplot(profile, ax=None, err=None, **kwargs):
         Passed to 'matplotlib.pyplot.plot'
     """
     dis, X = profile.dis, profile.X
-    clw = {'c': 'b', 'lw': 2}
+    clw = {'lw': 2}
     args = {**clw, **kwargs}
     if ax is None:
         fig = plt.figure()
@@ -37,14 +67,14 @@ def profileplot(profile, ax=None, err=None, **kwargs):
         Xp = np.zeros((2, len(disp)))
         for i in range(2):
             Xp[i] = splev(disp, profiles[i])
-        ax.plot(disp, Xp[0], 'r--', lw=2, label='Error')
-        ax.plot(disp, Xp[1], 'r--', lw=2)
+        p = ax.plot(disp, Xp[0], '--', lw=2, label='Error')
+        ax.plot(disp, Xp[1], '--', c=p[0].get_color(), lw=2)
 
-    ax.set_xlabel('Distance (micron)', fontsize=15)
-    ax.set_ylabel('Mole fraction', fontsize=15)
+    ax.set_xlabel('Distance (micron)', fontsize=label_fontsize)
+    ax.set_ylabel('Mole fraction', fontsize=label_fontsize)
     ax.set_xlim(dis.min(), dis.max())
-    ax.set_ylim(X.min(), X.max())
-    ax.tick_params(labelsize=12)
+    ax.set_ylim(plot_lim(X.min(), X.max()))
+    ax.tick_params(labelsize=tick_fontsize)
 
 
 def SFplot(profile, time, Xlim=[], ax=None, **kwargs):
@@ -66,16 +96,16 @@ def SFplot(profile, time, Xlim=[], ax=None, **kwargs):
     """
     X = profile.X
     sf = SF(profile, time, Xlim)
-    clw = {'c': 'b', 'marker': '.', 'ls': 'none'}
+    clw = {'marker': '.', 'ls': 'none'}
     args = {**clw, **kwargs}
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111)
     ax.semilogy(X, sf, **args)
-    ax.set_xlabel('Mole fraction', fontsize=15)
-    ax.set_ylabel('Diffusion Coefficients '+'$\mathsf{(m^2/s)}$', fontsize=15)
-    ax.set_xlim(X.min(), X.max())
-    ax.tick_params(labelsize=12)
+    ax.set_xlabel('Mole fraction', fontsize=label_fontsize)
+    ax.set_ylabel('Diffusion Coefficients '+'$\mathsf{(m^2/s)}$', fontsize=label_fontsize)
+    ax.set_xlim(plot_lim(X.min(), X.max()))
+    ax.tick_params(labelsize=tick_fontsize)
 
 
 def DCplot(diffsys, ax=None, err=None, **kwargs):
@@ -97,7 +127,7 @@ def DCplot(diffsys, ax=None, err=None, **kwargs):
         fig = plt.figure()
         ax = fig.add_subplot(111)
     Np, Xr, fD = diffsys.Np, diffsys.Xr, diffsys.Dfunc
-    clw = {'c': 'b', 'lw': 2}
+    clw = {'lw': 2}
     args = {**clw, **kwargs}
     clw_nl = {'label': '_nolegend_'}
     args_nl = {**args, **clw_nl}
@@ -119,13 +149,13 @@ def DCplot(diffsys, ax=None, err=None, **kwargs):
             pid = np.where((loc >= Xr[i, 0]) & (loc <= Xr[i, 1]))[0]
             Dloc = np.exp(splev(loc[pid], fD[i]))
             if i == 0:
-                ax.semilogy(loc[pid], Dloc * 10**errors[pid, 0], 'r--', lw=2, label='Error')
+                p = ax.semilogy(loc[pid], Dloc * 10**errors[pid, 0], '--', lw=2, label='Error')
             else:
-                ax.semilogy(loc[pid], Dloc * 10**errors[pid, 0], 'r--', lw=2)
-            ax.semilogy(loc[pid], Dloc * 10**errors[pid, 1], 'r--', lw=2)
+                p = ax.semilogy(loc[pid], Dloc * 10**errors[pid, 0], '--', lw=2)
+            ax.semilogy(loc[pid], Dloc * 10**errors[pid, 1], '--', c=p[0].get_color(), lw=2)
 
-    ax.set_xlim(Xr[0, 0], Xr[-1, 1])
-    ax.set_ylim(Dmin/10, Dmax*10)
-    ax.set_xlabel('Mole fraction', fontsize=15)
-    ax.set_ylabel('Diffusion Coefficients '+'$\mathsf{(m^2/s)}$', fontsize=15)
-    ax.tick_params(labelsize=12)
+    ax.set_xlim(plot_lim(Xr[0, 0], Xr[-1, 1]))
+    ax.set_ylim(plot_lim(Dmin, Dmax, True))
+    ax.set_xlabel('Mole fraction', fontsize=label_fontsize)
+    ax.set_ylabel('Diffusion Coefficients '+'$\mathsf{(m^2/s)}$', fontsize=label_fontsize)
+    ax.tick_params(labelsize=tick_fontsize)
